1141d6941d8a15d6a969137ce09c85293385e95e
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-01-07 00:08:33 -0600

    Se agrega carpeta y el archivo index para el tema de destructor

---
+++ destructor/index.php
@@ -0,0 +1,20 @@
+<?php
+	class Persona{
+		public function __construct(){
+			//No se recomienda imprimir en el constructor
+			//esto solo se hace con cuestiones didacticas
+			echo "Aqui se construye un objeto<br>";
+		}
+
+		public function __destruct(){
+			echo "Aqui se destruye el objeto pues ya no será invocado";
+		}
+	}
+
+	$persona = new Persona();
+
+	for ($i=1; $i <25 ; $i++) { 
+		// code...
+		echo $i."<br>";
+	}
+?>
